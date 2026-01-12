64f4228a64c6f8bbff1f1260d9af4cecf4f86fea
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-13 00:35:14 +0100

    ajudstements

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const AIRCONTROL_FORCE = 800.0
-const JUMP_VELOCITY = -150.0
+const JUMP_VELOCITY = -350.0
 const ABOX_ACCEL = 750
 
 const GRAVITY = 700.0
@@ -15,9 +15,9 @@ func _physics_process(delta: float) -> void:
 	
 	
 	## Add the gravity.
-	#	velocity += get_gravity() * delta
+	velocity += get_gravity() * delta
 	##Custom gravity so I can easily change const value, might go back to get gravity and change the project parameter when adjusted
-	velocity.y += GRAVITY * delta
+	#velocity.y += GRAVITY * delta
 
 
 	if is_on_floor():
@@ -44,7 +44,6 @@ func _physics_process(delta: float) -> void:
 
 #jump handler
 func box_jump(angle : float):
-	#print("\n\n velocity.length = ", velocity.length())
 	if abs(velocity.length())<abs(JUMP_VELOCITY):
 		velocity.x += -sin(angle)*JUMP_VELOCITY
 		velocity.y = cos(angle)*JUMP_VELOCITY
@@ -52,4 +51,5 @@ func box_jump(angle : float):
 		velocity.x += sin(angle)*velocity.length()
 		if abs(-cos(angle)*velocity.length())<abs(JUMP_VELOCITY):
 			velocity.y = cos(angle)*JUMP_VELOCITY
-		velocity.y = -cos(angle)*velocity.length()
+		else :
+			velocity.y = -cos(angle)*velocity.length()*0.75
