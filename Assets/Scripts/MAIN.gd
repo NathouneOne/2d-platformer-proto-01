af2f12e31961a9e1614a86b7368ab022f5b355c6
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-12 20:42:46 +0100

    added reload game F1 button

--- Assets/Scripts/MAIN.gd
+++ Assets/Scripts/MAIN.gd
@@ -28,6 +28,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("reload"):
+		get_tree().reload_current_scene()
 	
 	# Fix camera on y, allowing only X to move.
 	%Camera2D.global_position.y=0
